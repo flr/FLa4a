3e738fd42d814742e504f665859fe4c4b9329aaf
Ernesto <ernesto.jardim@jrc.ec.europa.eu>
2014-02-03 15:38:07 +0100

    now with biomass indices also fixed

--- R/fittingFunctions.R
+++ R/fittingFunctions.R
@@ -861,7 +861,7 @@ a4aInternal <- function(fmodel  = ~ s(age, k = 3) + factor(year),
     index <- lapply(1:length(indices), function(i) {
                       dmns <- dimnames(logq[[i]]) 
                       if (dmns $ age[1] == "all") {
-                        exp(logq[[i]] - center.log[1] + center.log[i+1]) * yearTotals(stock.n(a4aout)[, dmns[[2]]] * stock.wt(stock)[, dmns[[2]]])[1,]  
+                        exp(logq[[i]] - center.log[1] + center.log[i+1]) * yearTotals(stock.n(a4aout)[, dmns[[2]]] * exp(-Z*surveytime)[dmns[[1]], dmns[[2]]] * stock.wt(stock)[, dmns[[2]]])[1,]  
                       } else {
                         #stock.n(a4aout)[dmns[[1]], dmns[[2]]] * exp(logq[[i]]  - center.log[1] + center.log[i+1])
                         stock.n(a4aout)[dmns[[1]], dmns[[2]]] * exp(logq[[i]]  - center.log[1] + center.log[i+1])*exp(-Z*surveytime)[dmns[[1]], dmns[[2]]]
