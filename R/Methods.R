0c4f8ae1f522333433553a6bbabbb59b2def5797
colinpmillar <colinpmillar@googlemail.com>
2014-02-22 01:37:36 +0000

    added simulate method to simulate parameters from their MV normal distribution

--- R/Methods.R
+++ R/Methods.R
@@ -846,161 +846,176 @@ setMethod("genFLStock", c("a4aFitSA", "missing", "missing", "missing"),
 #' genFLQuant(harvest(ple4), method = "ac")
 setGeneric("simulate", useAsDefault = stats::simulate)
 
-#' @rdname coef-methods
-#' @aliases coef,FLa4aFit-method
-setMethod("simulate", signature(object = "a4aFitSA"),
-  function(object, nsim = 1, seed = NULL) {
+# #' @rdname coef-methods
+# #' @aliases coef,FLa4aFit-method
+# setMethod("simulate", signature(object = "a4aFitSA"),
+#   function(object, nsim = 1, seed = NULL) {
 
-    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) runif(1)
-    if (is.null(seed)) {
-        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
-    } else {
-        R.seed <- get(".Random.seed", envir = .GlobalEnv)
-        set.seed(seed)
-        RNGstate <- structure(seed, kind = as.list(RNGkind()))
-        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
-    }
+#     if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) runif(1)
+#     if (is.null(seed)) {
+#         RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+#     } else {
+#         R.seed <- get(".Random.seed", envir = .GlobalEnv)
+#         set.seed(seed)
+#         RNGstate <- structure(seed, kind = as.list(RNGkind()))
+#         on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+#     }
 
-    object <- pars(object)
+#     object <- pars(object)
 
-    years <- range(object @ stkmodel)[c("minyear","maxyear")]
-    ages <- range(object @ stkmodel)[c("min","max")]
+#     years <- range(object @ stkmodel)[c("minyear","maxyear")]
+#     ages <- range(object @ stkmodel)[c("min","max")]
   
-    #
-    # Build design matrix for catches only
-    #
-    full.df <- expand.grid(age  = ages[1]:ages[2],
-                           year = years[1]:years[2])[2:1]
+#     #
+#     # Build design matrix for catches only
+#     #
+#     full.df <- expand.grid(age  = ages[1]:ages[2],
+#                            year = years[1]:years[2])[2:1]
  
-#    if (!is.null(covar)) {
-#    # add in covariates to data.frame - it is easiest to provide covariates in one list
-#    tmp <- 
-#      lapply(seq_along(covar), 
-#        function(i) {
-#          x <- as.data.frame(covar[[i]])[c(1,2,7)]
-#          if (length(unique(x $ age)) == 1) x <- x[names(x) != "age"]
-#          if (length(unique(x $ year)) == 1) x <- x[names(x) != "year"]
-#          names(x) <- gsub("data", names(covar)[i], names(x))
-#          x
-#        })
-#    covar.df <- tmp[[1]]
-#    for (i in seq(length(covar) - 1)) covar.df <- merge(covar.df, tmp[[i + 1]], all = TRUE, sort = FALSE)
-#
-#    full.df <- merge(full.df, covar.df, all.x = TRUE, all.y = FALSE)
-#    } 
+# #    if (!is.null(covar)) {
+# #    # add in covariates to data.frame - it is easiest to provide covariates in one list
+# #    tmp <- 
+# #      lapply(seq_along(covar), 
+# #        function(i) {
+# #          x <- as.data.frame(covar[[i]])[c(1,2,7)]
+# #          if (length(unique(x $ age)) == 1) x <- x[names(x) != "age"]
+# #          if (length(unique(x $ year)) == 1) x <- x[names(x) != "year"]
+# #          names(x) <- gsub("data", names(covar)[i], names(x))
+# #          x
+# #        })
+# #    covar.df <- tmp[[1]]
+# #    for (i in seq(length(covar) - 1)) covar.df <- merge(covar.df, tmp[[i + 1]], all = TRUE, sort = FALSE)
+# #
+# #    full.df <- merge(full.df, covar.df, all.x = TRUE, all.y = FALSE)
+# #    } 
 
-    # make sure contrasts are set to sumto zero to match fit
-    opts <- options(contrasts = c(unordered = "contr.sum", ordered = "contr.poly")) 
+#     # make sure contrasts are set to sumto zero to match fit
+#     opts <- options(contrasts = c(unordered = "contr.sum", ordered = "contr.poly")) 
   
-    # f model matrix
-    Xf <- Matrix(getX(object @ stkmodel @ fMod, full.df))
+#     # f model matrix
+#     Xf <- Matrix(getX(object @ stkmodel @ fMod, full.df))
 
-    # initial age structure model matrix
-    Xny1 <- getX(object @ stkmodel @ n1Mod, subset(full.df, year == min(year) & age > min(age)))
+#     # initial age structure model matrix
+#     Xny1 <- getX(object @ stkmodel @ n1Mod, subset(full.df, year == min(year) & age > min(age)))
 
-    # Q model matrix  
-    fleet.names <- c("catch", names(object @ qmodel))
-    Xqlist <- lapply(seq_along(object @ qmodel), function(i) getX(object @ qmodel[[i]] @ Mod, subset(full.df, fleet == fleet.names[i+1])))
-    Xq <- as.matrix(do.call(bdiag, Xqlist))  
+#     # Q model matrix  
+#     fleet.names <- c("catch", names(object @ qmodel))
+#     Xqlist <- lapply(seq_along(object @ qmodel), function(i) getX(object @ qmodel[[i]] @ Mod, subset(full.df, fleet == fleet.names[i+1])))
+#     Xq <- as.matrix(do.call(bdiag, Xqlist))  
   
-    # var model matrix
-    Xvlist <- lapply(1:length(fleet.names), function(i) getX(object @ vmodel[[i]] @ Mod, subset(full.df, fleet == fleet.names[i])))
-    Xv <- as.matrix(do.call(bdiag, Xvlist))   
+#     # var model matrix
+#     Xvlist <- lapply(1:length(fleet.names), function(i) getX(object @ vmodel[[i]] @ Mod, subset(full.df, fleet == fleet.names[i])))
+#     Xv <- as.matrix(do.call(bdiag, Xvlist))   
     
-    # now separate the sr model element
-    facs <- strsplit(as.character(object @ stkmodel @ srMod)[length(object @ stkmodel @ srMod)], "[+]")[[1]]
-    facs <- gsub("(^ )|( $)", "", facs) # remove leading and trailing spaces
-    a4as <- grepl(paste("(^",c("bevholt", "ricker","hockey","geomean"),"[(])", collapse = "|", sep = ""), facs)
+#     # now separate the sr model element
+#     facs <- strsplit(as.character(object @ stkmodel @ srMod)[length(object @ stkmodel @ srMod)], "[+]")[[1]]
+#     facs <- gsub("(^ )|( $)", "", facs) # remove leading and trailing spaces
+#     a4as <- grepl(paste("(^",c("bevholt", "ricker","hockey","geomean"),"[(])", collapse = "|", sep = ""), facs)
 
-    # internal r model matrix
-    if (sum(a4as) == 0) rmodel <- object @ stkmodel @ srMod else rmodel <- ~ factor(year) 
-    Xr <- getX(rmodel, subset(full.df, age == min(age)))
+#     # internal r model matrix
+#     if (sum(a4as) == 0) rmodel <- object @ stkmodel @ srMod else rmodel <- ~ factor(year) 
+#     Xr <- getX(rmodel, subset(full.df, age == min(age)))
 
-    # reset options
-    options(opts)
+#     # reset options
+#     options(opts)
 
-    # always simulate from b distribution for SA class.  If you want fitted values do FLStock + a4aFit(a4aFitSA)
-    b.sim <- Matrix(simulate(object, nsim = nsim) @ stkmodel @ params @ .Data)
+#     # always simulate from b distribution for SA class.  If you want fitted values do FLStock + a4aFit(a4aFitSA)
+#     b.sim <- Matrix(simulate(object, nsim = nsim) @ stkmodel @ params @ .Data)
 
-    # matrix of predictions
-    Xbeta <- bdiag(Xf, Xny1, Xr) %*% b.sim
+#     # matrix of predictions
+#     Xbeta <- bdiag(Xf, Xny1, Xr) %*% b.sim
 
-    # plusgroup?
-    rng <- range(object @ stkmodel)
-    plusgrp <- !is.na(rng["plusgroup"]) && rng["plusgroup"] >= rng["max"]
+#     # plusgroup?
+#     rng <- range(object @ stkmodel)
+#     plusgrp <- !is.na(rng["plusgroup"]) && rng["plusgroup"] >= rng["max"]
 
-    # unpack m - good for recycling
-    Ms   <- c(m(object) @ .Data)
+#     # unpack m - good for recycling
+#     Ms   <- c(m(object) @ .Data)
  
-    # build stock
-    Fs <- Ns <- array(exp(Xbeta[1:nrow(Xf),]), dim = c(diff(ages)+1, diff(years)+1, ncol(Xbeta)))
-    Ns[] <- NA
-    Ns[-1,1,] <- array(exp(Xbeta[nrow(Xf) + 1:nrow(Xny1),]), dim = c(diff(ages), 1, ncol(Xbeta)))
-    Ns[1,,] <- array(exp(Xbeta[nrow(Xf) + nrow(Xny1) + 1:nrow(Xr),]), dim = c(1, diff(years)+1, ncol(Xbeta)))
-    Zs <- Fs + Ms
-    for (a in 2:dim(Ns)[1]) {
-      Ns[a,-1,] <- Ns[a-1, 1:diff(years),] * exp( - Zs[a-1, 1:diff(years),] )
-    }
-    # if plus group
-    if (plusgrp) {
-      for (y in 1:diff(years)) Ns[a,y+1,] <- Ns[a,y+1,] + Ns[a, y,] * exp( - Zs[a, y,] )
-    } 
-    # apply centering
-    Ns <- Ns * exp(object @ stkmodel @ centering)
+#     # build stock
+#     Fs <- Ns <- array(exp(Xbeta[1:nrow(Xf),]), dim = c(diff(ages)+1, diff(years)+1, ncol(Xbeta)))
+#     Ns[] <- NA
+#     Ns[-1,1,] <- array(exp(Xbeta[nrow(Xf) + 1:nrow(Xny1),]), dim = c(diff(ages), 1, ncol(Xbeta)))
+#     Ns[1,,] <- array(exp(Xbeta[nrow(Xf) + nrow(Xny1) + 1:nrow(Xr),]), dim = c(1, diff(years)+1, ncol(Xbeta)))
+#     Zs <- Fs + Ms
+#     for (a in 2:dim(Ns)[1]) {
+#       Ns[a,-1,] <- Ns[a-1, 1:diff(years),] * exp( - Zs[a-1, 1:diff(years),] )
+#     }
+#     # if plus group
+#     if (plusgrp) {
+#       for (y in 1:diff(years)) Ns[a,y+1,] <- Ns[a,y+1,] + Ns[a, y,] * exp( - Zs[a, y,] )
+#     } 
+#     # apply centering
+#     Ns <- Ns * exp(object @ stkmodel @ centering)
  
-    zfrac <- Fs / Zs * (1 - exp(-Zs))
+#     zfrac <- Fs / Zs * (1 - exp(-Zs))
 
-    dmns <- list(age    = paste(ages[1]:ages[2]), 
-                 year   = paste(years[1]:years[2]),
-                 unit   = "unique", 
-                 season = "all", 
-                 area   = "unique", 
-                 iter   = paste(seq(dim(b.sim)[2])))
+#     dmns <- list(age    = paste(ages[1]:ages[2]), 
+#                  year   = paste(years[1]:years[2]),
+#                  unit   = "unique", 
+#                  season = "all", 
+#                  area   = "unique", 
+#                  iter   = paste(seq(dim(b.sim)[2])))
                
-    dms <- unname(sapply(dmns, length))
+#     dms <- unname(sapply(dmns, length))
 
-    out <- FLStock(
-             stock.n = FLQuant(Ns, dim = dms, dimnames = dmns, units = stkmodel(object) @ units),
-             catch.n = FLQuant(zfrac * Ns, dim = dms, dimnames = dmns, units = stkmodel(object) @ units),
-             harvest = FLQuant(Fs, dim = dms, dimnames = dmns, units = "f"),
-             m       = m(object),
-             range   = object @ stkmodel @ range)
+#     out <- FLStock(
+#              stock.n = FLQuant(Ns, dim = dms, dimnames = dmns, units = stkmodel(object) @ units),
+#              catch.n = FLQuant(zfrac * Ns, dim = dms, dimnames = dmns, units = stkmodel(object) @ units),
+#              harvest = FLQuant(Fs, dim = dms, dimnames = dmns, units = "f"),
+#              m       = m(object),
+#              range   = object @ stkmodel @ range)
+
+#     out
+#   }
+# )
+
+
+
+
+#' @rdname vcov-methods
+#' @aliases vcov,FLa4aFit-method
+setMethod("simulate", signature(object = "a4aFitSA"),
+  function(object, nsim = 1, iter = NULL) {
+    vcov(object @ pars) <- value
+    object
+  })
+
+
+#' @rdname vcov-methods
+#' @aliases vcov,FLa4aFit-method
+setMethod("simulate", signature(object = "SCAPars"),
+  function(object, nsim = 1, iter = NULL) {    
+    out <- object
+    
+    out @ stkmodel <- simulate(object @ stkmodel, nsim = nsim, iter = iter)
+    out @ qmodel <- simulate(object @ qmodel, nsim = nsim, iter = iter)
+    out @ vmodel <- simulate(object @ vmodel, nsim = nsim, iter = iter)
 
     out
-  }
-)
+  })
 
 
-#' @rdname coef-methods
-#' @aliases coef,FLa4aFit-method
-setMethod("simulate", signature(object = "SCAPars"),
-  function(object, nsim = 1, seed = NULL, iter = NULL) {
-
-    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) runif(1)
-    if (is.null(seed)) {
-        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
-    } else {
-        R.seed <- get(".Random.seed", envir = .GlobalEnv)
-        set.seed(seed)
-        RNGstate <- structure(seed, kind = as.list(RNGkind()))
-        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
-    }
+#' @rdname vcov-methods
+#' @aliases vcov,FLa4aFitSA-method
+setMethod("simulate", signature(object = "a4aStkParams"),
+  function(object, nsim = 1, iter = NULL) {    
 
     # sanity checks
     if (is.null(iter)) {
-      if (nsim == 1) iter <- seq(dim(object @ stkmodel @ params)[2])
+      if (nsim == 1) iter <- seq(dim(object @ params)[2])
       if (nsim > 1) iter <- 1
     } else
     {
       if (nsim == 1) {
-        if (any(iter > dim(object @ stkmodel @ params)[2]) | any(iter < 0)) {
+        if (any(iter > dim(object @ params)[2]) | any(iter < 0)) {
           message("supplied values of iter are not sensible... simulating from all iters")
-          iter <- seq_along(dim(object @ stkmodel @ params)[2])
+          iter <- seq_along(dim(object @ params)[2])
         }
     } else
       {
         if (length(iter) > 1) stop("if nsim > 1 iter must be of length 1")
-        if (iter > dim(object @ stkmodel @ params)[2] | iter < 0) {
+        if (iter > dim(object @ params)[2] | iter < 0) {
           message("supplied values of iter are not sensible... simulating from iter = 1")
           iter <- 1
         }
@@ -1008,67 +1023,32 @@ setMethod("simulate", signature(object = "SCAPars"),
     }
     
     # get parameter estimates
-    stkpars <- object @ stkmodel @ params
-    qpars   <- lapply(object @ qmodel, function(x) x @ params)
-    vpars   <- lapply(object @ vmodel, function(x) x @ params)
+    b <- coef(object)
     
     # get parameter variance matrices
-    stkvars <- object @ stkmodel @ vcov
-    qvars   <- lapply(object @ qmodel, function(x) x @ vcov)
-    vvars   <- lapply(object @ vmodel, function(x) x @ vcov)
+    V <- vcov(object)
 
     # simulate some new params from the first iteration only!
-    if (dim(object @ stkmodel @ vcov)[3] == 1) {
+    if (dim(V)[3] == 1) {
       itervar <- rep(1, length(iter))
     } else {
       itervar = iter
     }
-    stkparsim <-
+    parsim <-
       sapply(seq_along(iter),
         function(i) 
-          t(mvrnorm(nsim, c(stkpars[,iter[i]]), stkvars[,,itervar[i]])))
+          t(mvrnorm(nsim, c(b[,iter[i]]), V[,,itervar[i]])))
 
-    qparsim <- 
-      lapply(seq_along(qpars), 
-        function(j) 
-          sapply(seq_along(iter), 
-            function(i) 
-              t(mvrnorm(nsim, c(qpars[[j]][,iter[i]]), qvars[[j]][,,itervar[i]]))))
-
-    vparsim <- 
-      lapply(seq_along(vpars), 
-        function(j) 
-          sapply(seq_along(iter), 
-            function(i) 
-              t(mvrnorm(nsim, c(vpars[[j]][,iter[i]]), vvars[[j]][,,itervar[i]]))))
     
     # load simpars into a SCAPars object and return
     out <- object
     
     if (nsim == 1) { 
-      out @ stkmodel @ params <- object @ stkmodel @ params
+      out @ params <- object @ params
     } else {
-      out @ stkmodel @ params <- propagate(object @ stkmodel @ params[,iter], nsim)
+      out @ params <- propagate(object @ params[,iter], nsim)
     }
-    out @ stkmodel @ params[] <- c(stkparsim)
-
-    for (i in seq_along(out @ qmodel)) {
-      if (nsim == 1) { 
-        out @ qmodel[[i]] @ params <- object @ qmodel[[i]] @ params
-      } else {
-        out @ qmodel[[i]] @ params <- propagate(object @ qmodel[[i]] @ params[,1], nsim)
-      }
-      out @ qmodel[[i]] @ params[] <- c(qparsim[[i]])
-    }
-
-    for (i in seq_along(out @ vmodel)) {
-      if (nsim == 1) { 
-        out @ vmodel[[i]] @ params <- object @ vmodel[[i]] @ params
-      } else {
-        out @ vmodel[[i]] @ params <- propagate(object @ vmodel[[i]] @ params[,1], nsim)
-      }
-      out @ vmodel[[i]] @ params[] <- c(vparsim[[i]])
-    }
+    out @ params[] <- c(parsim)
 
     ####
     # note we set the variance matrices to zero
@@ -1077,7 +1057,80 @@ setMethod("simulate", signature(object = "SCAPars"),
     vcov(out) <- 0
 
     return(out)
+})
+
+#' @rdname vcov-methods
+#' @aliases vcov,FLa4aFitSA-method
+setMethod("simulate", signature(object = "submodels"),
+  function(object, nsim = 1, iter = NULL) {
+    out <- lapply(object, simulate, nsim = nsim, iter = iter)
+    submodels(out)
   })
+
+
+#' @rdname coef-methods
+#' @aliases coef,FLa4aFit-method
+setMethod("simulate", signature(object = "submodel"),
+  function(object, nsim = 1, iter = NULL) {
+
+    # sanity checks
+    if (is.null(iter)) {
+      if (nsim == 1) iter <- seq(dim(object @ params)[2])
+      if (nsim > 1) iter <- 1
+    } else
+    {
+      if (nsim == 1) {
+        if (any(iter > dim(object @ params)[2]) | any(iter < 0)) {
+          message("supplied values of iter are not sensible... simulating from all iters")
+          iter <- seq_along(dim(object @ params)[2])
+        }
+    } else
+      {
+        if (length(iter) > 1) stop("if nsim > 1 iter must be of length 1")
+        if (iter > dim(object @ params)[2] | iter < 0) {
+          message("supplied values of iter are not sensible... simulating from iter = 1")
+          iter <- 1
+        }
+      }
+    }
+    
+    # get parameter estimates
+    b <- coef(object)
+    
+    # get parameter variance matrices
+    V <- vcov(object)
+
+    # simulate some new params from the first iteration of V only!
+    if (dim(V)[3] == 1) {
+      itervar <- rep(1, length(iter))
+    } else {
+      itervar = iter
+    }
+    parsim <- 
+      sapply(seq_along(iter), 
+            function(i) 
+              t(mvrnorm(nsim, c(b[,iter[i]]), V[,,itervar[i]])))
+
+    # load simpars into a submodel object and return
+    out <- object
+    
+    if (nsim == 1) { 
+      out @ params <- object @ params
+    } else {
+      out @ params <- propagate(object @ params[,iter], nsim)
+    }
+    out @ params[] <- c(parsim)
+
+    ####
+    # note we set the variance matrices to zero
+    # since having a variance no longer makes sense...
+    ####
+    vcov(out) <- 0
+
+    return(out)
+})
+
+
 
 
 
