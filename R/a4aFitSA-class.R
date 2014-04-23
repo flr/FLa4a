7cb94353d73bbdf0eea0368eb7cce73c1738d41f
Ernesto <ernesto.jardim@jrc.ec.europa.eu>
2014-04-23 15:07:17 +0200

    first model averaging function

--- R/a4aFitSA-class.R
+++ R/a4aFitSA-class.R
@@ -122,5 +122,78 @@ setMethod("pars", "a4aFitSA", function(object) object@pars)
 #' @aliases m,a4aFitSA-method
 setMethod("m", signature(object="a4aFitSA"), function(object) m(pars(object)))
 
+#====================================================================
+# plural class for a4aFitSA (used for model averaging)
+#====================================================================
+
+#' @rdname a4aFitSA-class
+#' @aliases a4aFitSAs-class
+
+setClass("a4aFitSAs", 
+	contains="FLComps",
+	validity=function(object){
+		if(!all(unlist(lapply(object, is, 'a4aFitSA'))))
+			return("Components must be a4aFitSA")	
+		return(TRUE)}
+)
+
+#' @rdname a4aFitSA-class
+#' @aliases a4aFitSAs a4aFitSAs,list-methods
+setGeneric("a4aFitSAs", function(object, ...) standardGeneric("a4aFitSAs"))
+setMethod("a4aFitSAs", signature(object="list"),
+  function(object, ...) {
+    args <- list(...)
+    
+    # names in args, ... 
+    if("names" %in% names(args)) {
+      names <- args[['names']]
+    } else {
+    # ... or in object,
+      if(!is.null(names(object))) {
+        names <- names(object)
+    # ... or in elements, ...
+      } else {
+        names <- unlist(lapply(object, name))
+        # ... or 1:n
+        idx <- names == "NA" | names == ""
+        if(any(idx))
+          names[idx] <- as.character(length(names))[idx]
+      }
+    }
+
+    # desc & lock
+    args <- c(list(Class="a4aFitSAs", .Data=object, names=names),
+      args[!names(args)%in%'names'])
+
+    return(
+      do.call('new', args)
+      )
+
+})
+
+#' @rdname a4aFitSA-class
+#' @aliases a4aFitSAs,a4aFitSA-methods
+setMethod("a4aFitSAs", signature(object="a4aFitSA"), function(object, ...) {
+    lst <- c(object, list(...))
+    a4aFitSAs(lst)
+})
+
+#' @rdname a4aFitSA-class
+#' @aliases a4aFitSAs a4aFitSAs,missing-methods
+setMethod("a4aFitSAs", signature(object="missing"),
+  function(...) {
+    # empty
+  	if(missing(...)){
+	  	new("a4aFitSAs")
+    # or not
+  	} else {
+      args <- list(...)
+      object <- args[!names(args)%in%c('names', 'desc', 'lock')]
+      args <- args[!names(args)%in%names(object)]
+      do.call('a4aFitSAs',  c(list(object=object), args))
+	  }
+  }
+)
+
 
 
---
+++ R/ma-methods.R
@@ -0,0 +1,48 @@
+#' @title Model averaging (experimental)
+#' @name ma
+#' @rdname ma-methods
+#' @description Method to average across a set of models. This is still experimental. Use with care.
+#' @param object a \code{a4aFits} object with the fits to be averaged across
+#' @param stock a \code{stock} object with the original data used for fitting
+#' @param fun a \code{function} that will be used to extract the values for weighting, for now it must be "AIC", "BIC" or "LogLik"
+#' @param nsim a \code{numeric} with the number os simulations to be drawn
+#' @return a \code{FLStock} object with iterations defined by \code{nsim}
+#' @aliases ma ma-methods ma,a4aFitSAs-method
+#' @examples
+#' data(ple4)
+#' data(ple4.indices)
+#' f1 <- sca(ple4, ple4.indices, fmodel=~ factor(age) + s(year, k=20), qmodel=list(~ s(age, k = 4), ~ s(age, k = 4), ~ s(age, k = 3)), fit = "assessment")
+#' f2 <- sca(ple4, ple4.indices, fmodel=~ factor(age) + s(year, k=20), qmodel=list(~ s(age, k = 4)+year, ~ s(age, k = 4), ~ s(age, k = 3)), fit = "assessment")
+#' stock.sim <- ma(a4aFits(list(f1, f2)), ple4, AIC, nsim = 1000)
+#' stks <- FLStocks(f1=ple4+f1, f2=ple4+f2, ma=stock.sim)
+#' flqs <- lapply(stks, ssb)
+#' flqs <- lapply(flqs, iterMedians)
+#' xyplot(data~year, groups=qname, data=flqs, type="l")
+#' plot(stks)
+setGeneric("ma", function(object, ...) standardGeneric("ma"))
+setMethod("ma", "a4aFitSAs", function(object, stock, FUN = AIC, nsim = 1000){
+
+  FUN <- match.fun(FUN)
+
+  # calculate weights
+  ICs <- -1 * sapply(object, FUN)
+  eICs <- exp( 0.5 * (ICs - max(ICs)))
+  weights <- eICs / sum(eICs)
+
+  wt.table <- data.frame("weight (perc)" = round(weights * 100, 3))
+  rownames(wt.table) <- names(object)
+  
+  message("model weights are \n\t", paste(capture.output(wt.table), collapse = "\n\t"))
+
+  # now sample from each model 1000 times and randomly select those to combine
+  sim <- sample(seq_along(object), nsim, prob = weights, replace = TRUE)
+
+  stock.sim <- propagate(stock, nsim)
+  for (i in seq_along(object)) {
+    if (sum(sim == i)) stock.sim[,,,,,sim == i] <- stock.sim[,,,,,sim == i] + object[[i]]
+  }
+  # DONE !!
+
+  stock.sim
+})
+
