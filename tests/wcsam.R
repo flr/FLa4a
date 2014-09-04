b8d0da360e0d596c83b37d805430e54721363afe
Ernesto <ernesto.jardim@jrc.ec.europa.eu>
2014-09-04 13:50:49 +0200

    WCSAM test fixed

--- tests/wcsam.R
+++ tests/wcsam.R
@@ -2,32 +2,32 @@ library(FLa4a)
 data(ple4)
 data(ple4.indices)
 err <- 0.05
-nits <- 10
+nits <- 250
 
 fit <-  a4aSCA(ple4, ple4.indices[1], qmodel=list(~s(age, k=4)))
-stk <- ple4 + fit
-fits <- simulate(fit, nits, 1234)
-stks <- propagate(ple4, nits) + fits 
-idxs <- ple4.indices[1]
-index(idxs[[1]]) <- index(fits)[[1]]
 
-fits <-  a4aSCA(stks, idxs, qmodel=list(~s(age, k=4)))
-stks2 <- stks + fits
+fit.sim <- simulate(fit, nits, 1234)
+
+idx.sim <- ple4.indices[1]
+index(idx.sim[[1]]) <- index(fit.sim)[[1]]
+stk.sim <- propagate(ple4, nits) + fit.sim 
+
+fit2 <-  a4aSCA(stk.sim, idx.sim, qmodel=list(~s(age, k=4)), fit="MP")
 
 # are the simulated values unbiased
 stk.fit <- stock.n(fit)
-stk.sim <- stock.n(stks2)
+stk.sim <- stock.n(fit2)
 stk.rat <- iterMedians(stk.sim)/stk.fit
 f.fit <- harvest(fit)
-f.sim <- harvest(stks2)
+f.sim <- harvest(fit2)
 f.rat <- iterMedians(f.sim)/f.fit
 cth.fit <- catch.n(fit)
-cth.sim <- catch.n(stks2)
+cth.sim <- catch.n(fit2)
 cth.rat <- iterMedians(cth.sim)/cth.fit
 
 max(stk.rat)-min(stk.rat) < err
 max(f.rat)-min(f.rat) < err
 max(cth.rat)-min(cth.rat) < err
 
-#plot(FLStocks(orig=stk, sim=stks, fitsim=stks+fits))
+
 
