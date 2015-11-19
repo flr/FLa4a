ef0532ebd8c1092afcac6686750a9386c40e1b4b
jaado <jose.deoliveira@cefas.co.uk>
2015-11-19 14:39:36 +0000

    Moderate edits
    Similar edits to a4aFit-class.R

--- man-roxygen/Example-a4aFitSA.R
+++ man-roxygen/Example-a4aFitSA.R
@@ -1,18 +1,21 @@
 #' @examples
 #' data(ple4)
 #' data(ple4.index)
+#'
 #' obj <- sca(stock=ple4, indices=FLIndices(ple4.index), fit="assessment")
 #' obj
+#'
+#' slotNames(obj)
 #' clock(obj)
 #' fitSumm(obj)
+#'
 #' flq <- stock.n(obj)
-#' is(flq)
-#' flq <- catch.n(obj)
-#' is(flq)
-#' flq <- harvest(obj)
 #' is(flq)
 #' flq <- index(obj)
 #' is(flq)
+#'
 #' logLik(obj)
+#' AIC(obj)
+#' BIC(obj)
+#'
 #' is(pars(obj))
-
