ad38c70e3e78c74c5c4c95fc1f52aa04080b7ece
jaado <jose.deoliveira@cefas.co.uk>
2015-11-27 16:36:46 +0000

    moderate
    Further elaboration on example for mvrnorm

--- R/a4aM-methods.R
+++ R/a4aM-methods.R
@@ -145,13 +145,24 @@ setMethod("m", "a4aM", function(object, grMod="missing", ...){
 #' mod1 <- FLModelSim(model=~exp(-age-0.5))
 #' mod2 <- FLModelSim(model=~k^0.66*t^0.57, params=FLPar(matrix(c(0.4,10,0.5,11),
 #'  ncol=2, dimnames=list(params=c("k","t"), iter=1:2))),
-#'  vcov=array(c(0.004,0.00,0.00,0.001), dim=c(2,2,2)))
+#'  vcov=array(c(0.004,0.,0.,0.001,0.006,0.,0.,0.003), dim=c(2,2,2)))
 #' mod3 <- FLModelSim(model=~1+b*v, params=FLPar(b=0.05))
 #' mObj <- a4aM(shape=mod1, level=mod2, trend=mod3,
 #'  range=c(min=0,max=15,minyear=2000,maxyear=2003,minmbar=0,maxmbar=0))
 #' mObj <- mvrnorm(100, mObj)
 #' # Generate 100 iterations with no trend over time
-#' m(mObj, v=c(1,1,1,1))
+#'   m(mObj, v=c(1,1,1,1))
+#' # Generate replicates based on iteration-specific multivariate distributions
+#' # (as defined by params() and vcov())
+#'   params(mod2)
+#'   vcov(mod2)
+#'   m1<-mvrnorm(mod2)
+#'   c(params(m1))
+#' # Generate replicates based on a single multivariate distribution (here the
+#' # median of params() and vcov() is used)
+#'   mvrnorm(2,mod2)
+#'   m2<-mvrnorm(2,mod2)
+#'   c(params(m2))
 
 setMethod("mvrnorm", c(n="numeric", mu="a4aM", Sigma="missing",
 	tol="missing", empirical="missing", EISPACK="missing"), function(n=1, mu) {
