6fa3d738152347f596289f828f16384925c29cce
Ernesto Jardim <ernesto.jardim@gmail.com>
2016-02-29 23:47:59 +0100

    fixed sr bug

--- R/fittingFunctions.R
+++ R/fittingFunctions.R
@@ -673,8 +673,7 @@ a4aInternal <- function(stock, indices, fmodel  = ~ s(age, k = 3) + factor(year)
 	if (ncol(Xsra) + ncol(Xsrb) > dims(stock)$year) stop("Stock recruitment model is over parameterised, please reduce the number parameters")
 
 	# internal r model matrix (this if is doing nothing)
-	#if (sum(a4as) == 0) rmodel <- srmodel else rmodel <- ~ factor(year) 
-	rmodel <- ~ factor(year) 
+	if (sum(a4as) == 0) rmodel <- srmodel else rmodel <- ~ factor(year) 
 	Xr <- getX(rmodel, subset(full.df, age == min(age) & fleet == "catch"))
 	#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
