32ab1443cffa67554625c1e607852da0beb3fe6a
Ernesto <ernesto.jardim@jrc.ec.europa.eu>
2016-03-22 14:37:08 +0100

    fixed bug in a4aInternal calling check.executable

--- R/fittingFunctions.R
+++ R/fittingFunctions.R
@@ -448,8 +448,8 @@ a4aInternal <- function(stock, indices, fmodel  = ~ s(age, k = 3) + factor(year)
 {
 
 	# first check permissions of executable
-	exeok <- check.executable()
-	if (!exeok) stop("a4a executable has wrong permissions.")
+#	exeok <- check.executable()
+#	if (!exeok) stop("a4a executable has wrong permissions.")
 
 	# if fit MCMC mcmc object must exist
 	if(fit=="MCMC" & missing(mcmc)) stop("To run MCMC you need to set the mcmc argument, using the method SCAMCMC.") 
