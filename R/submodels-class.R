7e4f852c87f1989b6dbf245e34628aa8365d4bd2
colin millar <colinpmillar@gmail.com>
2017-08-16 22:01:16 +0200

    simplify and add formula and show methods to submodels-class

--- R/submodels-class.R
+++ R/submodels-class.R
@@ -6,51 +6,33 @@
 #' @template ClassDescription
 #' @note This class is similar to other 'plural' calsses in \code{FLR}. It is a list constrained to having all elements of the same class, in this case \code{submodel}. Otherwise it works exacly as any other list.
 #' @aliases submodels-class
-setClass("submodels", contains = "FLComps")
-
-setValidity("submodels", 
-  function(object) {
-    # All items are submodel-class
-    if(!all(sapply(object, is, 'submodel'))) {
-      "Components must be submodel"
-    } else {
-      TRUE
-    }
-})
-
-
-
+submodels <- setClass("submodels", contains = "FLComps")
 
 #' @rdname submodels-class
 #' @template Constructors
 #' @template bothargs
 #' @aliases submodels submodels-methods
-setGeneric("submodels", function(object, ...)
-	standardGeneric("submodels"))
+setGeneric("submodels")
+
 #' @rdname submodels-class
-setMethod("submodels", signature(object="missing"),
+setMethod("submodels", "missing",
   function(...) {
     # empty
-  	if(missing(...)){
-	  	new("submodels")
+    if(missing(...)){
+      new("submodels")
     # or not
-  	} else {
+    } else {
       args <- list(...)
       object <- args[!names(args)%in%c('names', 'desc', 'lock')]
       args <- args[!names(args)%in%names(object)]
       do.call('submodels',  c(list(object=object), args))
-	  }
+    }
   }
 )
 
-#' @rdname submodels-class
-setMethod("submodels", signature(object="submodel"), function(object, ...) {
-    lst <- c(object, list(...))
-    submodels(lst)
-})
 
 #' @rdname submodels-class
-setMethod("submodels", signature(object="list"),
+setMethod("submodels", "list",
   function(object, ...) {
     
     args <- list(...)
@@ -74,13 +56,30 @@ setMethod("submodels", signature(object="list"),
 
     # desc & lock
     args <- c(list(Class="submodels", .Data=object, names=names),
-      args[!names(args)%in%'names'])
+      args[!names(args) %in% 'names'])
 
     return(
       do.call('new', args)
       )
+}) 
 
-}) # }}}
+
+setValidity("submodels", 
+  function(object) {
+    # All items are submodel-class
+    if(!all(sapply(object, is, 'submodel'))) {
+      "Components must be submodel"
+    } else {
+      TRUE
+    }
+})
+
+
+
+#
+#  accessor methods
+#
+
 
 #' @rdname submodels-class
 setMethod("params", "submodels", function(object) lapply(object, params))
@@ -92,11 +91,34 @@ setMethod("sMod", "submodels", function(object) lapply(object, sMod))
 setMethod("vcov", "submodels", function(object) lapply(object, vcov))
 
 #' @rdname submodels-class
+setMethod("formula", "submodels", function(x) lapply(x, formula))
+
+
+
+#
+#  show methods
+#
+
+setMethod("show", "submodels",
+  function(object)
+  {
+    cat("submodels:\n")
+    fmt <- paste0("\t %", max(nchar(sapply(object, name))), "smodel: ")
+    for (i in object) {
+      cat(sprintf(fmt, name(i))); print(formula(i), showEnv = FALSE)
+    }
+ })
+
+
+#
+# Other methods
+#
+
+#' @rdname submodels-class
 #' @param obj the object to be subset
 #' @param it iteration to be extracted 
 setMethod("iter", "submodels", function(obj, it){
-	out <- submodels(lapply(obj, iter, it))
-	names(out) <- names(obj)
-	out
+  out <- submodels(lapply(obj, iter, it))
+  names(out) <- names(obj)
+  out
 })
-
