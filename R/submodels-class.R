ad6e40b9ddb2789d99e8d01dee96421e073a2d94
colin millar <colinpmillar@gmail.com>
2017-10-01 11:30:14 +0200

    add iters and propagate to submodels

--- R/submodels-class.R
+++ R/submodels-class.R
@@ -6,8 +6,8 @@
 #' @template ClassDescription
 #' @note This class is similar to other 'plural' calsses in \code{FLR}. It is a list constrained to having all elements of the same class, in this case \code{submodel}. Otherwise it works exacly as any other list.
 #' @aliases submodels-class
-submodels <- 
-  setClass("submodels", 
+submodels <-
+  setClass("submodels",
     contains = "FLComps",
     slots = c("corBlocks" = "list"))
 
@@ -18,7 +18,7 @@ submodels <-
 setGeneric("submodels")
 
 setMethod("initialize", "submodels",
-  function(.Object, 
+  function(.Object,
            ...,
            corBlocks,
            names) {
@@ -47,23 +47,27 @@ setMethod("initialize", "submodels",
       } else {
         # generate from submodel dimensions
         nmodels <- length(.Object)
-        npar <- sapply(.Object, function(x) length(coef(x)))
-        parnames <- lapply(.Object, function(x) dimnames(coef(x))$params)
-        modelpairs <- combn(seq(nmodels), 2)
-        .Object@corBlocks <- 
-          lapply(seq(ncol(modelpairs)), 
-                 function(i) 
-                   matrix(0, 
-                          nrow = npar[modelpairs[1,i]],
-                          ncol = npar[modelpairs[2,i]],
-                          dimnames = parnames[modelpairs[,i]]))
-        names(.Object@corBlocks) <- apply(modelpairs, 2, function(x) paste(names(.Object)[x], collapse = "."))
+        # catch case of empty and single objects
+        if (nmodels > 1) {
+          npar <- sapply(.Object, function(x) length(coef(x)))
+          parnames <- lapply(.Object, function(x) dimnames(coef(x))$params)
+          modelpairs <- combn(seq(nmodels), 2)
+          .Object@corBlocks <-
+            lapply(seq(ncol(modelpairs)),
+                   function(i)
+                     array(0,
+                            dim = c(npar[modelpairs[1,i]],
+                                    npar[modelpairs[2,i]],
+                                    iter = 1),
+                            dimnames = c(parnames[modelpairs[,i]], "1")))
+          names(.Object@corBlocks) <- apply(modelpairs, 2, function(x) paste(names(.Object)[x], collapse = "."))
+        }
       }
       .Object
 })
 
 
-setValidity("submodels", 
+setValidity("submodels",
   function(object) {
     # All items are submodel-class
     if(!all(sapply(object, is, 'submodel'))) {
@@ -104,12 +108,12 @@ setMethod("formula", "submodels", function(x) lapply(x, formula))
 #
 
 #' @rdname submodels-class
-#' @value value the new object
+#' @param value value the new object
 #' @aliases corBlocks<-
 setGeneric("corBlocks<-", function(object, ..., value) standardGeneric("corBlocks<-"))
 
 #' @rdname vcov-methods
-#' @value value the new object
+#' @param value value the new object
 setMethod("corBlocks<-", signature(object = "submodels", value = "list"),
   function(object, ..., value) {
     object@corBlocks[] <- value
@@ -118,16 +122,16 @@ setMethod("corBlocks<-", signature(object = "submodels", value = "list"),
 
 # method.skeleton("$<-", signature(object = "submodels", value = "submodel"),  file = stdout())
 
-setMethod("$<-", 
+setMethod("$<-",
   signature(x = "submodels", value = "submodel"),
   function(x, name, value) {
     x[[name]] <- value
     x
-  })  
+  })
 
 setMethod("[[<-",
   c("submodels", "character", "missing"),
-  function (x, i, j, ..., value) 
+  function (x, i, j, ..., value)
   {
     lst <- as(x, "list")
     names(lst) <- names(x)
@@ -138,7 +142,7 @@ setMethod("[[<-",
 
 setMethod("[[<-",
   c("submodels", "numeric", "missing"),
-  function (x, i, j, ..., value) 
+  function (x, i, j, ..., value)
   {
     lst <- as(x, "list")
     names(lst) <- names(x)
@@ -156,9 +160,13 @@ setMethod("show", "submodels",
   function(object)
   {
     cat("submodels:\n")
-    fmt <- paste0("\t %", max(nchar(sapply(object, name))), "smodel: ")
-    for (i in object) {
-      cat(sprintf(fmt, name(i))); print(formula(i), showEnv = FALSE)
+    if (length(object) == 0) {
+      cat("empty object\n")
+    } else {
+      fmt <- paste0("\t %", max(nchar(sapply(object, name))), "smodel: ")
+      for (i in object) {
+        cat(sprintf(fmt, name(i))); print(formula(i), showEnv = FALSE)
+      }
     }
  })
 
@@ -172,7 +180,7 @@ setMethod("show", "submodels",
 
 setMethod("coerce",
   signature(from = "submodels", to = "submodel"),
-  function (from, to, strict = TRUE) 
+  function (from, to, strict = TRUE)
   {
     stop("No method described yet.")
   }
@@ -182,9 +190,50 @@ setMethod("coerce",
 # Other methods
 #
 
+
+# method.skeleton("propagate", "submodels",  file = stdout())
+#' @rdname submodels-class
+setMethod("propagate",
+  signature(object = "submodels"),
+  function (object, iter, fill.iter = TRUE)
+  {
+    if (length(object) == 0) {
+      stop("propagate can only extend a submodels object that has at least one submodel")
+    }
+
+    lst <- as(object, "list")
+    lst <- lapply(lst, propagate, iter = iter, fill.iter = fill.iter)
+
+    # propagate corblocks
+    corBlocks <-
+      lapply(object@corBlocks, function(x)
+      {
+        dob <- dim(x)
+
+        if (iter != dob[3]) {
+          # CHECK no iters in object
+          if (dob[3] > 1) stop("propagate can only extend objects with no iters")
+
+          out <- array(NA, dim = c(dob[1:2], iter), dimnames = c(dimnames(x)[1:2], list(1:iter)))
+          if (fill.iter) {
+            out[] <- as.vector(x)
+          } else {
+            out[,,1] <- as.vector(x)
+          }
+        } else {
+          out <- x
+        }
+        out
+      })
+
+    new("submodels", lst, corBlocks = corBlocks)
+  }
+)
+
+
 #' @rdname submodels-class
 #' @param obj the object to be subset
-#' @param it iteration to be extracted 
+#' @param it iteration to be extracted
 setMethod("iter", "submodels", function(obj, it){
   out <- submodels(lapply(obj, iter, it))
   names(out) <- names(obj)
