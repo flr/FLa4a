ae79e2f11d8f7a4e87df2b8d9eb1a564ab968d19
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2026-01-06 18:17:40 +0100

    Test commented out for the time being

--- tests/oe_tests.R
+++ tests/oe_tests.R
@@ -14,12 +14,12 @@ fits <- simulate(fit, nsim, seed=1234, obserror=TRUE)
 stk02 <- ple4 + fits
 idx02 <- ple4.indices + fits
 # add oe to catches using residual variance
-stk03 <- propagate(ple4, nsim) + res
+#stk03 <- propagate(ple4, nsim) + res
 # add oe to catches using residual variance
-stk04 <- stk01 + res
+#stk04 <- stk01 + res
 
 # plot
-stks <- FLStocks(s01 = stk01, s02 = stk02, s03 = stk03, s04 = stk04)
-cths <- lapply(stks, "slot", "catch.n")
+# stks <- FLStocks(s01 = stk01, s02 = stk02, s03 = stk03, s04 = stk04)
+# cths <- lapply(stks, "slot", "catch.n")
 
-system.time(fit02 <- sca(stk02, idx02))
+# system.time(fit02 <- sca(stk02, idx02))
